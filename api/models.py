89ae8f331ee5005e5d5dab68be1e99bdaa588400
neutralmike <neutralmike@yandex.ru>
2021-09-29 22:50:46 +0300

    models update

--- api/models.py
+++ api/models.py
@@ -6,7 +6,7 @@ class Item(models.Model):
     name = models.CharField(max_length=255, db_index=True)
     barcode = models.PositiveIntegerField()
     price = models.DecimalField(max_digits=10, decimal_places=2)
-    discount = models.PositiveSmallIntegerField(blank=True, null=True)
+    discount = models.PositiveSmallIntegerField(default=0)
 
 
 class Cart(models.Model):
@@ -28,6 +28,6 @@ class Cart(models.Model):
     ]
 
     app_mode = models.CharField(choices=APP_MODE_CHOICES, max_length=255)
-    cart_status = models.CharField(choices=STATUS_CHOICES, max_length=255)
+    status = models.CharField(choices=STATUS_CHOICES, max_length=255)
     created_at = models.DateField()
     items = models.ManyToManyField(Item)
